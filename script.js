1fbabc912e3fd2299774fd1ce14d3df21218c278
VerxMaxum <138268061+VerxMaxum@users.noreply.github.com>
2023-07-31 16:26:51 +0800

    Adjust display size and dot

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ displayableButtons.forEach(button => {
 const equals = document.querySelector('#result');
 equals.addEventListener('click', operate);
 
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', clear);
+
 function add(n1, n2) {
     return n1 + n2;
 }
@@ -38,8 +41,18 @@ function getOperator(displayText) {
             break;
         }
     }
-
     return operator;
+}
+
+function previousOperator(displayText) {
+    let hasPreviousOperator = false;
+    for(let i = 0; i < displayText.length; i++) {
+        if(displayText.charAt(i) === '+' || displayText.charAt(i) === '-' || displayText.charAt(i) === `\u{00F7}` || displayText.charAt(i) === `\u{00D7}`) {
+            hasPreviousOperator = true;
+            break;
+        }
+    }
+    return hasPreviousOperator;
 }
 
 function getSecondNumber(displayText) {
@@ -48,17 +61,28 @@ function getSecondNumber(displayText) {
     return numbers[1];
 }
 
+function isOperatorOrDot(text) {
+    return text === '+' || text === '-' || text === `\u{00F7}` || text === `\u{00D7}` || text === '.';
+}
+
 function display(event) {
     const button = event.target;
     const buttonText = button.textContent;
+    
+    if(isOperatorOrDot(buttonText) && isOperatorOrDot(displayText[displayText.length-1])) {
+        return;
+    }
+    if(isOperatorOrDot(buttonText) && previousOperator(displayText)) {
+        operate();
+    }
     displayText += buttonText;
     document.getElementById('display-text').textContent = displayText;
 }
 
 function operate() {
-    let firstNum = parseInt(getFirstNumber(displayText));
+    let firstNum = parseFloat(getFirstNumber(displayText));
     let operator = getOperator(displayText);
-    let secondNum = parseInt(getSecondNumber(displayText));
+    let secondNum = parseFloat(getSecondNumber(displayText));
     let result = 0;
 
     console.log(firstNum + ' ' + secondNum);
@@ -77,7 +101,15 @@ function operate() {
         result = multiply(firstNum, secondNum);
     }
 
-    displayText = result;
+    displayText = result.toString();
+    if(displayText.length > 15) {
+        displayText = displayText.substring(0,15);
+    }
     console.log("result = " + result);
+    document.getElementById('display-text').textContent = displayText;
+}
+
+function clear() {
+    displayText = "";
     document.getElementById('display-text').textContent = displayText;
 }
